76d72b6b8b94b1543093ebc953ee9cd22a20973f
mohamed-fathy <mohamedfathy573@gmail.com>
2024-07-10 15:23:02 +0300

    create-database-connection-setting-file

---
+++ web/sites/default/settings.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$databases['default']['default'] = [
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'root',
+  'host' => 'db',
+  'port' => '3306',
+  'driver' => 'mysql',
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+   ];
+
+   $config_directories = [
+    'CONFIG_SYNC_DIRECTORY' => 'sites/default/files/config_sync',
+  ];
+  
+  $settings['hash_salt'] = 'random-hash-salt';
+  
+  $settings['update_free_access'] = FALSE;
+  
+  $settings['file_private_path'] = 'sites/default/files/private';
+  
+  $settings['file_temp_path'] = 'sites/default/files/tmp';
